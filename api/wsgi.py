1f06727968576e714c16d25bcbb939e8cc6268c5
Amina-Delali <aimosta.official@gmail.com>
2025-03-12 16:49:27 +0100

    first commit-api name

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_first project.
+WSGI config for api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_first.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
 app =application
